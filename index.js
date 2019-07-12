4451190990716fe31c74f71cde2a30460ac65438
KINOKI <raphael@lesitevideo.net>
2019-07-13 00:00:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -21,25 +21,3 @@ sox.stderr.on('data', (data) => {
 sox.on('close', (code) => {
   console.log(`child process exited with code ${code}`);
 });
-
-
-
-/*const util = require('util');
-const exec = util.promisify(require('child_process').exec);
-
-const http = require('http');
-
-var streamURL = 'http://www.wefunkradio.com/play/radio.mp3';
-var freq = 107.9;
-
-async function main() {
-  const { stdout, stderr } = await exec('/usr/bin/sox -t mp3 '+streamURL+' -t wav - | sudo /home/pi/PiFmRds/src/pi_fm_rds -freq '+freq+' -ps KINOKI -audio -');
-
-  if (stderr) {
-    console.error(`error: ${stderr}`);
-  }
-  console.log(`Number of files ${stdout}`);
-}
-
-main()
-*/
